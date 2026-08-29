a9422fe48bb5c0fa2fcc3a6c2fc7540b7098ba85
lixiang <super_xiang@126.com>
2026-08-29 16:51:21 +0800

    ✨ feat: 添加 GitHub Contribution 贪吃蛇动画工作流与主页展示

--- README.md
+++ README.md
@@ -53,3 +53,12 @@
 
 
 *Last updated on: 2026-08-29 08:52:56*
+
+## Contribution Snake
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/loks666/loks666/output/github-contribution-grid-snake-dark.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/loks666/loks666/output/github-contribution-grid-snake.svg">
+  <img alt="github contribution grid snake animation" src="https://raw.githubusercontent.com/loks666/loks666/output/github-contribution-grid-snake.svg" width="100%" />
+</picture>
+
--- update.py
+++ update.py
@@ -203,6 +203,15 @@ def render(username: str, repos: list) -> str:
         )
 
     md += f"\n\n*Last updated on: {now}*\n"
+    md += f"""
+## Contribution Snake
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/{username}/{username}/output/github-contribution-grid-snake-dark.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/{username}/{username}/output/github-contribution-grid-snake.svg">
+  <img alt="github contribution grid snake animation" src="https://raw.githubusercontent.com/{username}/{username}/output/github-contribution-grid-snake.svg" width="100%" />
+</picture>
+"""
     return md
 
 
